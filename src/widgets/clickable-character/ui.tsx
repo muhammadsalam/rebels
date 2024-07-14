4487e1db89b66d084fce8eaf5244544466074119
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-07-14 16:40:41 +0300

    character shadows

--- src/widgets/clickable-character/ui.tsx
+++ src/widgets/clickable-character/ui.tsx
@@ -7,25 +7,18 @@ export const ClickableCharacter: FC<HTMLAttributes<HTMLDivElement>> = (
     const timeoutIdRef = useRef<number | null>(null);
 
     const handleOnTouchStart: TouchEventHandler<HTMLDivElement> = (e) => {
-        const event = e.targetTouches[0];
         const target = e.target as HTMLElement;
-        const rect = target.getBoundingClientRect();
-        const x = (event.clientX - rect.left) / rect.width - 0.5;
-        const y = 0.5 - (event.clientY - rect.top) / rect.height;
 
-        target.style.transform = `perspective(1500px) rotateX(${
-            y * 40
-        }deg) rotateY(${x * 40}deg)`;
+        target.classList.add(styles.wrapper__active);
 
         if (timeoutIdRef.current !== null) {
             clearTimeout(timeoutIdRef.current);
         }
 
         timeoutIdRef.current = window.setTimeout(() => {
-            target.style.transform =
-                "perspective(1500px) rotateX(0deg) rotateY(0deg)";
-            timeoutIdRef.current = null; // Сбросим идентификатор таймера после его завершения
-        }, 123);
+            target.classList.remove(styles.wrapper__active);
+            timeoutIdRef.current = null;
+        }, 323);
     };
 
     return (
