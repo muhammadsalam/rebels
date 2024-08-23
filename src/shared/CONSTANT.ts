392c6384ccdf52e02516f2fdfbf386b163763134
Muhammadsalam <muhammadsalam@list.ru>
2024-08-23 18:27:54 +0300

    constant changed

--- src/shared/CONSTANT.ts
+++ src/shared/CONSTANT.ts
@@ -1,11 +1,11 @@
-export const MAX_TOTAL_KNOWLEDGE_VALUE = 2310;
-export const MAX_TOTAL_LOYALTY_VALUE = 2280;
-export const MAX_TOTAL_INFLUENCE_VALUE = 2200;
+export const MAX_TOTAL_KNOWLEDGE_VALUE = 905;
+export const MAX_TOTAL_LOYALTY_VALUE = 299;
+export const MAX_TOTAL_INFLUENCE_VALUE = 827;
 
 export const MAX_TEAM_KNOWLEDGE = 231;
-export const MAX_TEAM_LOYALTY = 228;
+export const MAX_TEAM_LOYALTY = 72;
 export const MAX_TEAM_INFLUENCE = 220;
 
 export const MAX_CARD_KNOWLEDGE = 55;
-export const MAX_CARD_LOYALTY = 55;
+export const MAX_CARD_LOYALTY = 16;
 export const MAX_CARD_INFLUENCE = 55;
