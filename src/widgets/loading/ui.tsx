0ac4f941fddd7811fcae3bc565df7fc77309444d
Muhammadsalam <muhammadsalam@list.ru>
2024-08-25 18:47:10 +0300

    loading changed to img

--- src/widgets/loading/ui.tsx
+++ src/widgets/loading/ui.tsx
@@ -3,7 +3,8 @@ import styles from "./styles.module.scss";
 export const Loading = () => {
     return (
         <div className={styles.loading}>
-            <h1 className={styles.title}>Rebels</h1>
+            <img className={styles.img} src="/assets/loading.png" alt="REBELS" />
+            {/* <h1 className={styles.title}>Rebels</h1> */}
         </div>
     );
 };
