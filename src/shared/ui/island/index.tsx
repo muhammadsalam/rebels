b2a9f9c9053e7fdc6e39a7605e1e1f488646a2a6
Muhammadsalam <muhammadsalam@list.ru>
2024-08-25 17:49:37 +0300

    island component

---
+++ src/shared/ui/island/index.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+import { FC, HTMLAttributes, memo } from "react";
+import styles from './style.module.scss';
+import { LinkProps } from "react-router-dom";
+
+export const Island: FC<{
+    tag?: any,
+} & (LinkProps | HTMLAttributes<HTMLDivElement>)> = memo(({ tag: Tag = 'div', children, className, ...props }) => {
+    return (
+        <Tag className={clsx(className, styles.island)} {...props}>
+            {children}
+        </Tag>
+    );
+})
