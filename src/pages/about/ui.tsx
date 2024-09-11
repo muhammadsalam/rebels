21a9269724ab37971e980e35b9221d28aa257cc4
Muhammadsalam <muhammadsalam@list.ru>
2024-09-11 10:28:39 +0300

    about changed

--- src/pages/about/ui.tsx
+++ src/pages/about/ui.tsx
@@ -32,255 +32,113 @@ export const AboutPage = () => {
                 <br />
                 <h3 className={styles.h3}>1. Getting started</h3>
                 <br />
-                Characters and cards
+                Your goal is to collect hero cards, fight enemies, and earn points to upgrade cards and advance through levels. As you play, you'll strengthen your team and gain access to new opportunities.
+
+                <br />
+                <br />
+                <h3 className={styles.h3}>2. Hero characteristics and their impact</h3>
+                <br />
+                <ul className={styles.ul}>
+                    <li>Knowledge: affects damage per tap and the number of points for mining. The higher the Knowledge, the faster you defeat enemies and earn more points.</li>
+                    {/* <br /> */}
+                    <li>Loyalty: determines how much energy is spent per tap and affects mining duration. The higher the Loyalty, the less energy is consumed.</li>
+                    {/* <br /> */}
+                    <li>Influence: increases the chance of a critical hit. The higher the Influence, the more often heroes will deal critical damage, helping to destroy enemies faster.</li>
+                </ul>
+                <br />
+                How to upgrade heroes
+                <br />
+                <br />
+                To upgrade a hero, you need to have a duplicate card and the necessary number of points to pay for the upgrade. Upgrading increases the hero's characteristics, making them stronger.
+                <br />
+                <br />
+                Special upgrade feature for level 7 cards: When a hero reaches level 7, you can burn it along with its duplicate to obtain a random level 1 card of the next rarity.
+                <br />
+                <br />
+                <h3 className={styles.h3}>3. Chests and their cost</h3>
+                <br />
+                Chests are the main way to obtain new cards of the heroes. The higher the chest level, the greater the chances of getting rare cards:
                 <br />
                 <br />
                 <ul className={styles.ul}>
-                    <li>
-                        Characters: the game features 30 unique characters, each
-                        of whom has contributed to the world of cryptocurrency.
-                        Characters are divided into 5 rarities: Common,
-                        Uncommon, Rare, Epic, and Legendary
-                    </li>
-                    <br />
-                    <li>
-                        Cards: each character is represented as a card that can
-                        be upgraded to enhance the character's abilities. Card
-                        upgrades are done by merging identical cards and using
-                        game points
-                    </li>
+                    <li>A Common chest contains basic hero cards</li>
+                    <li>Rare, Epic, and Legendary chests give rarer and more powerful heroes</li>
                 </ul>
                 <br />
-                Starting team
+                Important: The price of chests increases by a fixed amount during the first 5 purchases, after which it stabilizes. This means that in the later stages of the game, the price of chests stabilizes, and you can plan your point spending more effectively.
+                <br />
+                <br />
+                <h3 className={styles.h3}>4. Fighting enemies</h3>
+                <br />
+                To defeat an enemy, you form a team of 5 heroes and deal damage with taps:
                 <br />
                 <br />
                 <ul className={styles.ul}>
-                    <li>
-                        New players start with 5 identical level 1 Common cards.
-                        You can upgrade them, obtain new cards, and create your
-                        ideal team
-                    </li>
+                    <li>Damage per tap: depends on the total Knowledge of your team's heroes</li>
+                    <li>Energy: each attack spends energy. Heroes' Loyalty reduces energy consumption, allowing for longer participation in battle.</li>
+                    <li>Critical hits: the chance of a critical hit increases with growing Influence, which helps deal more damage.</li>
                 </ul>
                 <br />
-                <h3 className={styles.h3}>2. Energy and battle</h3>
+                Form a balanced team. Use heroes with high Knowledge for increased damage and Influence for frequent critical hits. If energy runs out too quickly, add heroes with high Loyalty to reduce consumption.
                 <br />
-                Energy
+                <br />
+                <h3 className={styles.h3}>5. Mining</h3>
+                <br />
+                Mining allows you to earn points in alongside with other activities in the game:
                 <br />
                 <br />
                 <ul className={styles.ul}>
-                    <li>
-                        Base energy consumption: each action in the game
-                        requires 100 energy units, which you spend with each
-                        "tap."
-                    </li>
-                    <br />
-                    <li>
-                        Loyalty influence: the higher the Loyalty of the
-                        character, the less energy is required for their
-                        actions
-                    </li>
+                    <li>Amount of mining points: depends on the team's Knowledge and Influence.</li>
+                    <li>Mining duration: depends on Loyalty. The higher the Loyalty, the longer heroes can mine.</li>
                 </ul>
                 <br />
-                Damage and tap
+                <h3 className={styles.h3}>6. Account level and its impact</h3>
+                <br />
+                Account level grows as you upgrade your heroes. As you level up, you get more energy and access to new chests.
+                <br />
+                <br />
+                Total volume and energy recovery per second depending on account level:
                 <br />
                 <br />
                 <ul className={styles.ul}>
-                    <li>Base damage per tap depends on the card rarity:</li>
-                    <ul>
-                        <li>Common: 10 damage</li>
-                        <li>Uncommon: 20 damage</li>
-                        <li>Rare: 30 damage</li>
-                        <li>Epic: 40 damage</li>
-                        <li>Legendary: 50 damage</li>
-                    </ul>
+                    <li>Level 1: 20,000 energy, recovering 12 energy units per second</li>
+                    <li>Level 2: 21,000 energy, recovering 14 energy units per second</li>
+                    <li>Level 3: 22,000 energy, recovering 16 energy units per second</li>
+                    <li>Level 4: 23,000 energy, recovering 18 energy units per second</li>
+                    <li>Level 5: 24,000 energy, recovering 20 energy units per second</li>
+                    <li>Level 6: 25,000 energy, recovering 22 energy units per second</li>
+                    <li>Level 7: 26,000 energy, recovering 24 energy units per second</li>
+                    <li>Level 8: 27,000 energy, recovering 26 energy units per second</li>
+                    <li>Level 9: 28,500 energy, recovering 28 energy units per second</li>
+                    <li>Level 10: 30,000 energy, recovering 30 energy units per second</li>
                 </ul>
                 <br />
-                <h3 className={styles.h3}>3. Chests and rewards</h3>
+                <h3 className={styles.h3}>7. Referral system</h3>
                 <br />
-                Types of chests
+                Invite friends to receive percentages of their earnings:
                 <br />
                 <br />
                 <ul className={styles.ul}>
-                    <li>Initial chest costs:</li>
-                    <ul>
-                        <li>Common: 20.000 points</li>
-                        <li>Uncommon: 50.000 points</li>
-                        <li>Rare: 100.000 points</li>
-                        <li>Epic: 150.000 points</li>
-                        <li>Legendary: 250.000 points</li>
-                    </ul>
+                    <li>Tier 1: 2% of referrals' earnings</li>
+                    <li>Tier 2: 4% — 25 people</li>
+                    <li>Tier 3: 6% — 225 people</li>
+                    <li>Tier 4: 8% — 525 people</li>
+                    <li>Tier 5: 10% — 975 people</li>
                 </ul>
+                <br />
+                The more people you invite, the higher your referral tier and the more income you'll receive from your referrals’ activities.
+                <br />
+                <br />
+                <h3 className={styles.h3}>Summary</h3>
                 <br />
                 <ul className={styles.ul}>
-                    <li>Chest price increase:</li>
-                    <ul>
-                        <li>Level 1 Chest - 10%</li>
-                        <li>Level 2 Chest - 10%</li>
-                        <li>Level 3 Chest - 15%</li>
-                        <li>Level 4 Chest - 15%</li>
-                        <li>Level 5 Chest - 20%</li>
-                    </ul>
+                    <li>Compose balanced teams for maximum damage to the enemy and conserving your own energy.</li>
+                    <li>Upgrade heroes to increase account level and unlock more valuable chests.</li>
+                    <li>Use mining for additional passive income.</li>
+                    <li>Progress in account levels to increase energy and access to rare cards.</li>
+                    <li>Invite friends to increase referral income.</li>
                 </ul>
-                <br />
-                Opening chests
-                <br />
-                <br />
-                <ul className={styles.ul}>
-                    <li>
-                        Players earn points in battles and can spend them to
-                        purchase chests. With each opening, the cost of the next
-                        chest increases, so choose wisely!
-                    </li>
-                </ul>
-                <br />
-                <h3 className={styles.h3}>4. Card Upgrades</h3>
-                <br />
-                Upgrading
-                <br />
-                <br />
-                To upgrade a card, you need to combine it with another identical
-                card. For example, if you have a level 3 Epic card and a level 1
-                Epic card, combining them will give you a level 4 Epic card
-                <br />
-                <br />
-                Upgrading cards of level 7: if you have a level 7 Common card
-                and a level 1 Common card, they can be burned for free to
-                receive a random level 1 card of the next rarity (in this case,
-                Uncommon)
-                <br />
-                <br />
-                Upgrade costs
-                <br />
-                <br />
-                Common
-                <ul className={styles.ul}>
-                    <li>Level 1 -{">"} Level 2: 15.000 points</li>
-                    <li>Level 2 -{">"} Level 3: 18.750 points</li>
-                    <li>Level 3 -{">"} Level 4: 23.500 points </li>
-                    <li>Level 4 -{">"} Level 5: 29.500 points</li>
-                    <li>Level 5 - {">"} Level 6: 36.700 points</li>
-                    <li>Level 6 -{">"} Level 7: 45.800 points</li>
-                </ul>
-                <br />
-                Uncommon
-                <ul className={styles.ul}>
-                    <li>Level 1 -{">"} Level 2: 37.500 points</li>
-                    <li>Level 2 -{">"} Level 3: 46.900 points</li>
-                    <li>Level 3 -{">"} Level 4: 58.600 points</li>
-                    <li>Level 4 -{">"} Level 5: 73.300 points</li>
-                    <li>Level 5 -{">"} Level 6: 91.600 points</li>
-                    <li>Level 6 -{">"} Level 7: 114.500 points</li>
-                </ul>
-                <br />
-                Rare
-                <ul className={styles.ul}>
-                    <li>Level 1 -{">"} Level 2: 75.000 points</li>
-                    <li>Level 2 -{">"} Level 3: 93.750 points</li>
-                    <li>Level 3 -{">"} Level 4: 117.100 points</li>
-                    <li>Level 4 -{">"} Level 5: 146.500 points</li>
-                    <li>Level 5 -{">"} Level 6: 183.200 points</li>
-                    <li>Level 6 -{">"} Level 7: 228.900 points</li>
-                </ul>
-                <br />
-                Epic
-                <ul className={styles.ul}>
-                    <li>Level 1 -{">"} Level 2: 112.500 points</li>
-                    <li>Level 2 -{">"} Level 3: 140.700 points</li>
-                    <li>Level 3 -{">"} Level 4: 175.800 points</li>
-                    <li>Level 4 -{">"} Level 5: 219.800 points</li>
-                    <li>Level 5 -{">"} Level 6: 274.700 points</li>
-                    <li>Level 6 -{">"} Level 7: 343.400 points</li>
-                </ul>
-                <br />
-                Legendary
-                <ul className={styles.ul}>
-                    <li>Level 1 -{">"} Level 2: 150.000 points</li>
-                    <li>Level 2 -{">"} Level 3: 187.500 points</li>
-                    <li>Level 3 -{">"} Level 4: 234.400 points</li>
-                    <li>Level 4 -{">"} Level 5: 293.000 points</li>
-                    <li>Level 5 -{">"} Level 6: 366.400 points</li>
-                    <li>Level 6 -{">"} Level 7: 458.000 points</li>
-                </ul>
-                <br />
-                <h3 className={styles.h3}>5. Account development</h3>
-                <br />
-                Account level
-                <br />
-                <br />
-                The account level is determined by the total experience of all
-                cards and affects the volume and recovery speed of energy.
-                <br />
-                <br />
-                Levels and their characteristics:
-                <ul className={styles.ul}>
-                    <li>
-                        Level 1: 0-95 experience, 12 units per second, 20.000
-                        energy
-                    </li>
-                    <li>
-                        Level 2: 96-199 experience, 14 units per second, 21.000
-                        energy
-                    </li>
-                    <li>
-                        Level 3: 200-349 experience, 16 units per second, 22.000
-                        energy
-                    </li>
-                    <li>
-                        Level 4: 350-529 experience, 18 units per second, 23.000
-                        energy
-                    </li>
-                    <li>
-                        Level 5: 530-749 experience, 20 units per second, 24.000
-                        energy
-                    </li>
-                    <li>
-                        Level 6: 750-1009 experience, 22 units per second,
-                        25.000 energy
-                    </li>
-                    <li>
-                        Level 7: 1010-1309 experience, 24 units per second,
-                        26.000 energy
-                    </li>
-                    <li>
-                        Level 8: 1310-1649 experience, 26 units per second,
-                        27.000 energy
-                    </li>
-                    <li>
-                        Level 9: 1650-2030 experience, 28 units per second,
-                        28.500 energy
-                    </li>
-                    <li>
-                        Level 10: 2031+ experience, 30 units per second, 30.000
-                        energy
-                    </li>
-                </ul>
-                <br />
-                <h3 className={styles.h3}>6. Mining basics</h3>
-                <br />
-                <ul className={styles.ul}>
-                    <li>Influence and Knowledge: These attributes affect the speed of point mining. The higher the combined Influence and Knowledge of your team, the more points you earn per mining cycle</li>
-                    <br />
-                    <li>Loyalty: Increases the duration of the mining cycle, allowing you to earn points for a longer period without needing to restart the process</li>
-                </ul>
-                <br />
-                <h3 className={styles.h3}>7. Strategies and tips</h3>
-                <br />
-                <ul className={styles.ul}>
-                    <li>
-                        Balanced team: choose characters with different skills
-                        and levels
-                    </li>
-                    <br />
-                    <li>
-                        Smart investments: decide which cards to upgrade and
-                        when to buy chests
-                    </li>
-                    <br />
-                    <li>
-                        Play regularly: the more active you are, the faster you
-                        can accumulate points and progress in the game
-                    </li>
-                </ul>
+
             </div>
         </div>
     );
