9a204afa90bea94dc4087a566ed673667a17d8ef
Muhammadsalam <muhammadsalam@list.ru>
2024-09-19 16:50:58 +0300

    lvl up fix

--- src/widgets/level-up-modal/index.tsx
+++ src/widgets/level-up-modal/index.tsx
@@ -25,7 +25,7 @@ export const LevelUpModal: FC = () => {
 
                 await setTimeout(() => {
                     setIsLevelUpgradedShow(false);
-                }, 3500);
+                }, 3000);
 
                 useUserStore.setState({ prev_level: level });
             }
