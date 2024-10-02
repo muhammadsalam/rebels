bb8db04a6cb79dfee94cb7253c1a16da0e553ebc
Muhammadsalam <muhammadsalam@list.ru>
2024-10-02 10:13:55 +0300

    daily gift changed

--- src/shared/CONSTANT.ts
+++ src/shared/CONSTANT.ts
@@ -1,3 +1,16 @@
 export const MAX_CARD_KNOWLEDGE = 55;
 export const MAX_CARD_LOYALTY = 16;
 export const MAX_CARD_INFLUENCE = 55;
+
+export const our_frens = [
+    "DWF",
+    "BLUM",
+    "NOT",
+    "MEMEFI",
+    "GRAFUN",
+    "TEST1",
+    "TEST2",
+    "TEST3",
+    "TEST4",
+    "TEST5",
+] as const;
--- src/widgets/modal-gift/ui/index.tsx
+++ src/widgets/modal-gift/ui/index.tsx
@@ -1,99 +1,101 @@
-import { FC, useState } from "react";
+import { FC, memo, useState } from "react";
 import { Modal } from "shared/ui";
 import styles from "./styles.module.scss";
-import GiftIcon from "icons/gift.svg?react";
 import { axios, showAlert } from "shared/libs/utils";
 import { useGameStatsStore, useUserStore } from "entities/user";
 import { TReward } from "widgets/modal-reward";
 import { useHeroStore } from "entities/heroes";
 import useSound from "use-sound";
+import { our_frens } from "shared/CONSTANT";
 
 export const ModalGift: FC<{
     onModalHide: () => void;
     setReward: (reward: number | TReward | null) => void;
     setIsRewardModalActive: (_: boolean) => void;
     setIsDailyGiftActive: (_: boolean) => void;
-}> = ({
+}> = memo(({
     onModalHide,
     setReward,
     setIsRewardModalActive,
     setIsDailyGiftActive,
 }) => {
-        const sounds = useUserStore(state => state.settings.sounds)
-        const [isDailySending, setIsDailySending] = useState(false);
-        const [playGiftSound] = useSound("/assets/sounds/gift.mp3");
+    const sounds = useUserStore(state => state.settings.sounds)
+    const [isDailySending, setIsDailySending] = useState(false);
+    const [playGiftSound] = useSound("/assets/sounds/gift.mp3");
 
-        const onCardClick = async (index: number) => {
-            try {
-                console.log(isDailySending);
-                sounds && playGiftSound();
-                if (isDailySending) return;
-                setIsDailySending(true);
+    const onCardClick = async (fren_name: string) => {
+        try {
+            console.log(isDailySending);
+            sounds && playGiftSound();
+            if (isDailySending) return;
+            setIsDailySending(true);
 
-                const { status, data } = await axios.get(`/spin/run?${index}`);
+            const { status, data } = await axios.get(`/spin/run?${fren_name}`);
 
-                if (status !== 200) {
-                    setIsRewardModalActive(false);
-                    throw new Error("Something went wrong. Please try again later");
-                }
-
-                setIsRewardModalActive(true);
-                setIsDailyGiftActive(false);
-
-                if (data.prize.toLowerCase() === "points") {
-                    setReward(data.points);
-                    useUserStore.setState({
-                        balance: data.balance,
-                    });
-                }
-
-                if (data.prize.toLowerCase() === "hero") {
-                    setReward(data.hero);
-                    useHeroStore.setState({ cards: data.heroes });
-                    useHeroStore.getState().teamFromCards();
-                    useGameStatsStore.setState({
-                        next_level_value: data.profile.next_level_value,
-                        start_level_value: data.profile.start_value,
-                        total_value: data.profile.total_value,
-                        total_values: data.profile.total_values,
-                    });
-                    useUserStore.setState({
-                        level_name: data.profile.level_name,
-                        level: data.profile.level,
-                    });
-                }
-
-                useGameStatsStore.setState({
-                    energy_update: data.energy_update,
-                    max_energy: data.max_energy,
-                    daily_available_at: (+Date.now() + 24 * 60 * 60 * 1000) / 1000,
-                });
-
-                setIsDailySending(false);
-            } catch (e) {
-                showAlert("Something went wrong. Please try again later. " + e);
-
-                setIsDailySending(false);
+            if (status !== 200) {
+                setIsRewardModalActive(false);
+                throw new Error("Something went wrong. Please try again later");
             }
-        };
 
-        return (
-            <Modal
-                heading="Daily gift"
-                subheading="Choose your option"
-                onModalHide={onModalHide}
-            >
-                <div className={styles.cards}>
-                    {new Array(4).fill(0).map((_, index) => (
-                        <div
-                            key={index}
-                            className={styles.card}
-                            onClick={() => onCardClick(index + 1)}
-                        >
-                            <GiftIcon />
-                        </div>
-                    ))}
-                </div>
-            </Modal>
-        );
+            setIsRewardModalActive(true);
+            setIsDailyGiftActive(false);
+
+            if (data.prize.toLowerCase() === "points") {
+                setReward(data.points);
+                useUserStore.setState({
+                    balance: data.balance,
+                });
+            }
+
+            if (data.prize.toLowerCase() === "hero") {
+                setReward(data.hero);
+                useHeroStore.setState({ cards: data.heroes });
+                useHeroStore.getState().teamFromCards();
+                useGameStatsStore.setState({
+                    next_level_value: data.profile.next_level_value,
+                    start_level_value: data.profile.start_value,
+                    total_value: data.profile.total_value,
+                    total_values: data.profile.total_values,
+                });
+                useUserStore.setState({
+                    level_name: data.profile.level_name,
+                    level: data.profile.level,
+                });
+            }
+
+            useGameStatsStore.setState({
+                energy_update: data.energy_update,
+                max_energy: data.max_energy,
+                daily_available_at: (+Date.now() + 24 * 60 * 60 * 1000) / 1000,
+            });
+
+            setIsDailySending(false);
+        } catch (e) {
+            showAlert("Something went wrong. Please try again later. " + e);
+
+            setIsDailySending(false);
+        }
     };
+
+    const gift_cards = [0, 0, 0, 0].map(() => our_frens[Math.floor(Math.random() * our_frens.length)]);
+
+    return (
+        <Modal
+            heading="Daily gift"
+            subheading="Choose your option"
+            onModalHide={onModalHide}
+        >
+            <div className={styles.cards}>
+                {gift_cards.map((fren_name, index) => (
+                    <div
+                        key={index}
+                        className={styles.card}
+                        onClick={() => onCardClick(fren_name)}
+                    >
+                        <img src={`/assets/images/frens/${fren_name}.png`} alt="" />
+                    </div>
+                ))}
+            </div>
+        </Modal>
+    );
+});
